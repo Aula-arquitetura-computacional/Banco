2f60a54e4d5c1f36fe9ff155919df3fcbd2cecb3
Will Dantas <126539225+dantaswilljpg@users.noreply.github.com>
2023-05-02 16:00:39 -0300

    Update banco projeto.sql

--- banco projeto.sql
+++ banco projeto.sql
@@ -8,19 +8,17 @@ CREATE TABLE lager(
 idLager INT PRIMARY KEY AUTO_INCREMENT,
 data_hora DATETIME DEFAULT current_timestamp,
 processo_maceracao FLOAT,
-processo_malteracao FLOAT,
+processo_malteacao_etapa1 FLOAT,
+processo_malteacao_etapa2 FLOAT,
+processo_malteacao_etapa3 FLOAT,
 processo_moagem FLOAT,
-processo__brassagem FLOAT,
+processo_resfriamento_etapa1 FLOAT,
+processo_resfriamento_etapa2 FLOAT,
+processo_resfriamento_etapa3 FLOAT,
 processo_fervura FLOAT,
-processo_resfriamento FLOAT,
+processo_brassagem_etapa1 FLOAT,
+processo_brassagem_etapa2 FLOAT,
+processo_brassagem_etapa3 FLOAT,
 processo_maturacao FLOAT,
-processo_pasterizacao FLOAT,
-processo_distribuicao FLOAT
-);
-
-
-INSERT INTO lager VALUES
-(NULL ,default , 20.3 , 25.7 , 29.1 , 32.4 , 85.2 , 12.4 , 26.2 , 23.2 , 21.8 );
-
-
-SELECT processo_distribuicao FROM LAGER;
+processo_pasteurizacao FLOAT,
+processo_distribuicao float);
